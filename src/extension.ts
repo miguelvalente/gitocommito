6f210e85e9f7525844343a2d58a06fdf09906ba6
miguelvalente <miguelvalente@protonmail.com>
2023-06-09 11:27:39 +0200

    feat: :sparkles: Add new command

--- src/extension.ts
+++ src/extension.ts
@@ -1,43 +1,22 @@
 import * as vscode from 'vscode';
-import { getFilteredStagedChanges } from './gitCommands';
+import { getFilteredStagedChanges, getFilteredUnstagedChanges } from './gitCommands';
 
 export function activate(context: vscode.ExtensionContext) {
-    let disposable = vscode.commands.registerCommand('extension.generateCommitMessage', async () => {
+    let generateCommitMessageStaged = vscode.commands.registerCommand('extension.generateCommitMessageStaged', async () => {
         // Here you can use the functions from gitCommands.ts, e.g.
         const stagedChanges = await getFilteredStagedChanges("ACDMRTUB");
-        console.log(stagedChanges["D"]);
-		console.log("mesa")
+        console.log(stagedChanges);
     });
 
-    context.subscriptions.push(disposable);
+    let generateCommitMessageUnstaged = vscode.commands.registerCommand('extension.generateCommitMessageUnstaged', async () => {
+        // Here you can use the functions from gitCommands.ts, e.g.
+        const unstagedChanges = await getFilteredUnstagedChanges("ACDMRTUB");
+        console.log(unstagedChanges);
+        console.log(unstagedChanges);
+    });
+
+
+    context.subscriptions.push(generateCommitMessageStaged, generateCommitMessageUnstaged);
 }
 
 export function deactivate() {}
-
-
-// // The module 'vscode' contains the VS Code extensibility API
-// // Import the module and reference it with the alias vscode in your code below
-// import * as vscode from 'vscode';
-
-// // This method is called when your extension is activated
-// // Your extension is activated the very first time the command is executed
-// export function activate(context: vscode.ExtensionContext) {
-
-// 	// Use the console to output diagnostic information (console.log) and errors (console.error)
-// 	// This line of code will only be executed once when your extension is activated
-// 	console.log('Congratulations, your extension "gitocommito" is now active!');
-
-// 	// The command has been defined in the package.json file
-// 	// Now provide the implementation of the command with registerCommand
-// 	// The commandId parameter must match the command field in package.json
-// 	let disposable = vscode.commands.registerCommand('gitocommito.helloWorld', () => {
-// 		// The code you place here will be executed every time your command is executed
-// 		// Display a message box to the user
-// 		vscode.window.showInformationMessage('Hello World from gitocommito!');
-// 	});
-
-// 	context.subscriptions.push(disposable);
-// }
-
-// // This method is called when your extension is deactivated
-// export function deactivate() {}
--- src/gitCommands.ts
+++ src/gitCommands.ts
@@ -1,37 +1,30 @@
 import { exec } from 'child_process';
 import * as fs from 'fs';
 
-export function runGitCommand(cmd: string[]): Promise<string> {
-    return new Promise((resolve, reject) => {
-        exec(cmd.join(' '),
-            (error: any, stdout: string, stderr: string) => {
-                if (error) {
-                    reject(stderr);
-                } else {
-                    resolve(stdout);
-                }
-            }
-        );
-    });
-}
 
-export async function getUnstagedChangesDiff(filterType: string): Promise<[string, string]> {
-    let cmd = ['git', 'diff', '--diff-filter', filterType];
-    let diffOutput = await runGitCommand(cmd);
+export async function getUnstagedChangesDiff(filterType: string, directory: string): Promise<[string, string]> {
+    let cmd = ['git', 'diff', '--diff-filter=' + filterType];
+    let diffOutput = await runGitCommand(cmd, directory);
 
     if (filterType === 'U') {
         let cmdUntracked = ['git', 'ls-files', '--others', '--exclude-standard'];
-        let untrackedFiles = (await runGitCommand(cmdUntracked)).split('\n');
+        let untrackedFiles = (await runGitCommand(cmdUntracked, directory)).split('\n').filter(file => file.trim() !== ''); // filter out empty strings
         for (let file of untrackedFiles) {
+            const fullPath = `${directory}/${file}`; // Provide the full path
             diffOutput += `\nUntracked file: ${file}\n`;
-            diffOutput += fs.readFileSync(file, 'utf8').split('\n').map(line => `+${line}`).join('\n');
+            try {
+                diffOutput += fs.readFileSync(fullPath, 'utf8').split('\n').map(line => `+${line}`).join('\n');
+            } catch (err) {
+                console.error(`Error reading file ${fullPath}: ${err}`);
+            }
         }
     }
     return [filterType, diffOutput];
 }
 
+
 export async function getFilteredUnstagedChanges(filterTypes: string = 'ACDMRTUB') {
-    let diffPromises = filterTypes.split('').map(filterType => getUnstagedChangesDiff(filterType));
+    let diffPromises = filterTypes.split('').map(filterType => getUnstagedChangesDiff(filterType, '/home/mvalente/deving/gitocommito'));
 
     let unstagedChangesDiff: { [key: string]: string } = {};
 
@@ -44,14 +37,51 @@ export async function getFilteredUnstagedChanges(filterTypes: string = 'ACDMRTUB
     return unstagedChangesDiff;
 }
 
-export async function getStagedChangesDiff(filterType: string): Promise<[string, string]> {
-    let cmd = ['git', 'diff', '--cached', '--diff-filter', filterType];
-    let diffOutput = await runGitCommand(cmd);
-    return [filterType, diffOutput];
+export function runGitCommand(cmd: string[], directory: string): Promise<string> {
+    return new Promise((resolve, reject) => {
+        console.log(`Running command: ${cmd.join(' ')} in directory: ${directory}`);
+        exec(
+            cmd.join(' '),
+            { cwd: directory },
+            (error: any, stdout: string, stderr: string) => {
+                if (error) {
+                    console.error(`Error executing command: ${stderr}`);
+                    reject(stderr);
+                } else {
+                    resolve(stdout);
+                }
+            }
+        );
+    });
+}
+async function isGitRepo(directory: string): Promise<boolean> {
+    try {
+        // Attempt to run a git status command; will fail if not in a git repo.
+        await runGitCommand(['git', 'status'], directory);
+        return true; // If it doesn't throw an error, it's a git repo.
+    } catch (error) {
+        return false; // If it throws an error, it's not a git repo.
+    }
+}
+
+
+export async function getStagedChangesDiff(filterType: string, directory: string): Promise<[string, string]> {
+    try {
+        if (!await isGitRepo(directory)) {
+            throw new Error('Not inside a Git repository');
+        }
+    
+        let cmd = ['git', 'diff', '--cached', '--diff-filter=' + filterType];
+        let diffOutput = await runGitCommand(cmd, directory);
+        return [filterType, diffOutput];
+    } catch (error) {
+        console.error(`Error in getStagedChangesDiff: ${error}`);
+        throw error;
+    }
 }
 
 export async function getFilteredStagedChanges(filterTypes: string = 'ACDMRTUB') {
-    let diffPromises = filterTypes.split('').map(filterType => getStagedChangesDiff(filterType));
+    let diffPromises = filterTypes.split('').map(filterType => getStagedChangesDiff(filterType, '/home/mvalente/deving/gitocommito'));
 
     let stagedChangesDiff: { [key: string]: string } = {};
 
