04e9568843efe8d09047fafa5fcb95c1de6fe2a4
miguelvalente <miguelvalente@protonmail.com>
2023-06-12 09:55:08 +0200

    chore: :coffin: Delete prints

--- src/extension.ts
+++ src/extension.ts
@@ -5,14 +5,11 @@ export function activate(context: vscode.ExtensionContext) {
     let generateCommitMessageStaged = vscode.commands.registerCommand('extension.generateCommitMessageStaged', async () => {
         // Here you can use the functions from gitCommands.ts, e.g.
         const stagedChanges = await getFilteredStagedChanges("ACDMRTUB");
-        console.log(stagedChanges);
     });
 
     let generateCommitMessageUnstaged = vscode.commands.registerCommand('extension.generateCommitMessageUnstaged', async () => {
         // Here you can use the functions from gitCommands.ts, e.g.
         const unstagedChanges = await getFilteredUnstagedChanges("ACDMRTUB");
-        console.log(unstagedChanges);
-        console.log(unstagedChanges);
     });
 
 
