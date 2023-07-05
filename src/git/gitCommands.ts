d9444eb0ba7c95984484744991c3eaae09f7e577
miguelvalente <miguelvalente@protonmail.com>
2023-07-05 08:19:52 +0200

    refactor: :wastebasket: Move runGitCommand function
    The runGitCommand function was moved from runGitCommands.ts to gitCommands.ts, so the old implementation in runGitCommands.ts is no longer needed.

--- src/git/gitCommands.ts
+++ src/git/gitCommands.ts
@@ -1,7 +1,28 @@
 // getStagedChanges.ts
 // getStagedChanges.ts
-import { runGitCommand } from "./runGitCommands";
 import * as vscode from "vscode";
+import { exec } from "child_process"; // you need to import exec from the correct package
+
+
+function runGitCommand(
+  cmd: string[],
+  directory: string
+): Promise<string> {
+  return new Promise((resolve, reject) => {
+    exec(
+      cmd.join(" "),
+      { cwd: directory },
+      (error: any, stdout: string, stderr: string) => {
+        if (error) {
+          console.error(`Error executing command: ${stderr}`);
+          reject(stderr);
+        } else {
+          resolve(stdout);
+        }
+      }
+    );
+  });
+}
 
 async function isGitRepo(directory: string): Promise<boolean> {
   try {
